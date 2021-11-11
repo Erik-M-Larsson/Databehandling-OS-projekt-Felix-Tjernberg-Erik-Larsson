1cb0840e25afa96bacb81af3ffe8785a27c4b383
filleflex <felix.tjernberg@hotmail.se>
2021-11-11 15:38:17 +0100

    Start implementation of sport data frames

---
+++ dash/components/sport/load_sport_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+class SportDataFrame:
+    def __init__(self, sport_name: str) -> None:
+        pass
--- dash/components/sport/sport.py
+++ dash/components/sport/sport.py
@@ -1,13 +1,15 @@
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
+from components.sport.load_sport_data import load_sport_data
+
 
 layout = html.Div(
     [
         html.H1(id="sport-name"),
+        html.P(id="sport-data-frame"),
         dcc.Link("Back", href="/"),
     ]
 )
@@ -17,3 +19,10 @@ layout = html.Div(
 def parse_path_name(pathname):
     split_path_name = pathname.split("/")
     return split_path_name[2]
+
+
+@app.callback(Output("sport-data-frame", "children"), Input("url", "pathname"))
+def load_data_frame(pathname):
+    split_path_name = pathname.split("/")
+    data_frame = load_sport_data(split_path_name[2])
+    return data_frame
