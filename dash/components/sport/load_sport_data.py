7cce8ee9bd5a8ac429c5d84043bc8f59bb564076
FilleFLEX <felix.tjernberg@hotmail.se>
2021-11-12 10:05:48 +0100

    Complete implementation of sport data frames

--- dash/components/sport/load_sport_data.py
+++ dash/components/sport/load_sport_data.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
 
-class SportDataFrame:
-    def __init__(self, sport_name: str) -> None:
-        pass
+def load_sport_data_frames():
+    os_data_raw = pd.read_csv("./data/athlete_events.csv")
+
+    create_sport_data_frame = lambda sport_name: os_data_raw.query(
+        f'Sport == "{sport_name}"'
+    )
+
+    sport_dict = {
+        sport: create_sport_data_frame(sport)
+        for sport in list(os_data_raw["Sport"].unique())
+    }
+
+    return sport_dict
--- dash/components/sport/sport.py
+++ dash/components/sport/sport.py
@@ -1,28 +1,47 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly_express as px
+import pandas as pd
 from dash.dependencies import Input, Output
 
 from app import app
-from components.sport.load_sport_data import load_sport_data
+from components.sport.load_sport_data import load_sport_data_frames
 
+sport_data_frames = load_sport_data_frames()
 
 layout = html.Div(
     [
+        dcc.Store(id="sport-data-frame"),
         html.H1(id="sport-name"),
-        html.P(id="sport-data-frame"),
+        dcc.Graph(id="sport-histogram"),
         dcc.Link("Back", href="/"),
     ]
 )
 
 
-@app.callback(Output("sport-name", "children"), Input("url", "pathname"))
-def parse_path_name(pathname):
-    split_path_name = pathname.split("/")
-    return split_path_name[2]
-
-
-@app.callback(Output("sport-data-frame", "children"), Input("url", "pathname"))
+@app.callback(Output("sport-data-frame", "data"), Input("url", "pathname"))
 def load_data_frame(pathname):
     split_path_name = pathname.split("/")
-    data_frame = load_sport_data(split_path_name[2])
-    return data_frame
+    sport_data_frame = sport_data_frames[f"{split_path_name[2].capitalize()}"]
+    return sport_data_frame.to_json()
+
+
+@app.callback(Output("sport-name", "children"), Input("url", "pathname"))
+def update_page_title(pathname):
+    split_path_name = pathname.split("/")
+    return split_path_name[2].capitalize()
+
+
+@app.callback(
+    Output("sport-histogram", "figure"),
+    Input("sport-data-frame", "data"),
+)
+def update_histogram_graph(data):
+    sport_data_frame = pd.read_json(data)
+    figure = px.histogram(
+        sport_data_frame,
+        x="Age",
+        labels={"count": "Antal"},
+        title="Age distribution",
+    )
+    return figure
