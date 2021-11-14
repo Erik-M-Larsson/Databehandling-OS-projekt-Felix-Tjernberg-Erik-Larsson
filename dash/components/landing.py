ff7160099ffdf16a895c55ca4e7bd3f6f65aa44e
FilleFLEX <felix.tjernberg@hotmail.se>
2021-11-14 17:57:03 +0100

    Style landing page for large screens

--- dash/components/landing.py
+++ dash/components/landing.py
@@ -9,41 +9,59 @@ layout = html.Div(
     [
         html.H1("Sweden is best per capita*"),
         html.Aside([html.P("* Best per capita in top 10 of countries")]),
-        html.H2("World medal race top 10"),
-        dcc.Graph(id="world-medal-race"),
-        html.H2("Sweden medals per year"),
-        dcc.Dropdown(
-            clearable=False,
-            id="sport-dropdown",
-            options=[
-                {"label": f"Year {number}", "value": "placeholder"}
-                for number in range(4)
+        html.Section(
+            [
+                html.Div(
+                    [
+                        html.H2("World medal race top 10"),
+                        dcc.Graph(id="world-medal-race"),
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.H2("Sweden medals per year"),
+                        dcc.Dropdown(
+                            clearable=False,
+                            id="sport-dropdown",
+                            options=[
+                                {"label": f"Year {number}", "value": "placeholder"}
+                                for number in range(4)
+                            ],
+                            value="Year 0",
+                        ),
+                        dcc.Graph(id="sweden-medals-at-year"),
+                    ]
+                ),
             ],
-            value="Year 0",
+            id="landing-graphs",
         ),
-        dcc.Graph(id="sweden-medals-at-year"),
         html.H2("Sweden fun facts"),
-        html.Div(
+        html.Section(
             [
-                html.H2(
-                    "Our youngest medal taker is Nils skoglund!",
+                html.Div(
+                    [
+                        html.H3(
+                            "Sweden's youngest medal",
+                        ),
+                        html.P(
+                            "Nils skoglund at the age of 13 took silver in diving year 1920",
+                        ),
+                    ],
+                    className="glass-background",
                 ),
-                html.P(
-                    "At the age of 13 he took silver in diving year 1920",
+                html.Div(
+                    [
+                        html.H3(
+                            "Sweden's oldest medal",
+                        ),
+                        html.P(
+                            "Oscar Gomer Swahn at the age of 72 took silver in shooting year 1920",
+                        ),
+                    ],
+                    className="glass-background",
                 ),
             ],
-            className="glass-background",
-        ),
-        html.Div(
-            [
-                html.H2(
-                    "Our oldest medal taker is Oscar Gomer Swahn!",
-                ),
-                html.P(
-                    "At the age of 72 took silver in shooting year 1920",
-                ),
-            ],
-            className="glass-background",
+            id="fun-facts",
         ),
     ],
     id="landing",
