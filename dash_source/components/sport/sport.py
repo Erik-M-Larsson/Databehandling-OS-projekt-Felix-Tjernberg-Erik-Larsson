2f985af6579cf94ecb3195b48ef45852970abc96
Erik-M-Larsson <89965231+Erik-M-Larsson@users.noreply.github.com>
2021-11-15 12:04:07 +0100

    Rename dash folder

---
+++ dash_source/components/sport/sport.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import plotly_express as px
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+from app import app
+from components.sport.load_sport_data import load_sport_data_frames
+from components.sport.sport_list import sport_list
+import components.sport.plots as plots
+
+
+sport_data_dictionaries = load_sport_data_frames()
+
+
+anatomy_filename = "assets/anatomy.svg"
+
+medals_filename = "assets/os-medals.svg"
+
+layout = html.Div(
+    [
+        dcc.Store(id="sport-data-frame"),
+        dcc.Store(id="sport-medal-frame"),
+        html.Header(
+            [
+                html.H1(id="sport-name"),
+                dcc.Dropdown(
+                    clearable=False,
+                    id="sport-dropdown",
+                    options=[
+                        {"label": f"{sport}", "value": sport} for sport in sport_list
+                    ],
+                    value="Cycling"
+                    # placeholder="Select a sport",
+                ),
+            ]
+        ),
+        html.H2("Medal race"),
+        html.Img(
+            id="medals-svg",
+            src=medals_filename,
+            alt="Illustration of silver, gold and bronze olympic medals",
+        ),
+        dcc.Graph(id="sport-medal-race"),
+        html.H2("Player type"),
+        html.Section(
+            [
+                html.Img(
+                    id="anatomy-svg",
+                    src=anatomy_filename,
+                    alt="Illustration of female and male anatomy",
+                ),
+                dcc.Graph(id="sport-gender-pie"),
+                dcc.Graph(id="sport-age-histogram"),
+                html.Div(
+                    [
+                        dcc.Graph(id="sport-height-histogram"),
+                        dcc.Graph(id="sport-weight-histogram"),
+                    ],
+                    id="height-and-width-histograms",
+                ),
+            ],
+            id="player-type",
+        ),
+    ],
+    id="sport",
+)
+
+
+@app.callback(Output("sport-data-frame", "data"), Input("sport-dropdown", "value"))
+def load_data_frame(value):
+    if value == None:
+        return
+
+    return sport_data_dictionaries[f"{format(value.title())}"]["general"].to_json()
+
+
+@app.callback(Output("sport-medal-frame", "data"), Input("sport-dropdown", "value"))
+def load_medal_frame(value):
+    if value == None:
+        return
+
+    return sport_data_dictionaries[f"{format(value.title())}"]["medal_count"].to_json()
+
+
+@app.callback(Output("sport-name", "children"), Input("sport-dropdown", "value"))
+def update_heading_text(value):
+    if value == None:
+        return "Sport stats"
+
+    return value
+
+
+@app.callback(
+    Output("sport-age-histogram", "figure"),
+    Input("sport-data-frame", "data"),
+)
+def update_age_histogram(data):
+    if data == None:
+        return px.histogram()
+
+    sport_data_frame = pd.read_json(data)
+    return plots.age_histogram(sport_data_frame)
+
+
+@app.callback(
+    Output("sport-gender-pie", "figure"),
+    Input("sport-data-frame", "data"),
+)
+def update_gender_pie(data):
+    if data == None:
+        return px.pie()
+
+    sport_data_frame = pd.read_json(data)
+    return plots.gender_pie(sport_data_frame)
+
+
+@app.callback(
+    Output("sport-height-histogram", "figure"),
+    Input("sport-data-frame", "data"),
+)
+def update_height_histogram(data):
+    if data == None:
+        return px.histogram()
+
+    sport_data_frame = pd.read_json(data)
+    return plots.height_histogram(sport_data_frame)
+
+
+@app.callback(
+    Output("sport-weight-histogram", "figure"),
+    Input("sport-data-frame", "data"),
+)
+def update_weight_histogram(data):
+    if data == None:
+        return px.histogram()
+
+    sport_data_frame = pd.read_json(data)
+    return plots.weight_histogram(sport_data_frame)
+
+
+@app.callback(
+    Output("sport-medal-race", "figure"),
+    Input("sport-medal-frame", "data"),
+)
+def update_medal_race(data):
+    if data == None:
+        return px.bar()
+
+    sport_data_frame = pd.read_json(data)
+    return plots.medal_race_plot(sport_data_frame)
---
+++ dash_source/index.py
@@ -0,0 +1,44 @@
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+from app import app
+from components import landing
+from components.sport import sport
+
+
+os_rings_filename = "assets/os-rings.svg"
+
+app.layout = html.Div(
+    [
+        html.Nav(
+            [
+                html.Img(src=os_rings_filename, alt="Image of olympic rings"),
+                html.Ul(
+                    [
+                        html.Li([html.A("Home", href="/")]),
+                        html.Li([html.A("Sport stats", href="/sport-stats")]),
+                    ]
+                ),
+            ]
+        ),
+        html.Main(
+            [
+                dcc.Location(id="url", refresh=False),
+                html.Article(id="page-content"),
+            ]
+        ),
+    ],
+)
+app.title = "Olympic sports dash app"
+
+
+@app.callback(Output("page-content", "children"), Input("url", "pathname"))
+def display_page(path_name):
+    if path_name == "/sport-stats":
+        return sport.layout
+    else:
+        return landing.layout
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
