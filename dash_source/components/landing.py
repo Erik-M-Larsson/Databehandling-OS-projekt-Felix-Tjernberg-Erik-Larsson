6f6f9776330ff087773779236b4f92e8b6781ddb
filleflex <felix.tjernberg@hotmail.se>
2021-11-15 17:50:30 +0100

    Add top 10 world medal race

--- dash_source/components/landing.py
+++ dash_source/components/landing.py
@@ -1,12 +1,18 @@
 from dash import html
 from dash import dcc, html
 from dash.dependencies import Input, Output
+import pandas as pd
+import plotly_express as px
 
 from app import app
+from components.sport.load_sport_data import create_medal_count_data_frame
+from components.sport.plots import medal_race_plot
 
+os_data_raw = pd.read_csv("./data/athlete_events.csv")
 
 layout = html.Div(
     [
+        dcc.Store(id="world-medal-frame"),
         html.H1("Sweden is best per capita*"),
         html.Aside([html.P("* Best per capita in top 10 of countries")]),
         html.Section(
@@ -22,7 +28,7 @@ layout = html.Div(
                         html.H2("Sweden medals per year"),
                         dcc.Dropdown(
                             clearable=False,
-                            id="sport-dropdown",
+                            id="year-dropdown",
                             options=[
                                 {"label": f"Year {number}", "value": "placeholder"}
                                 for number in range(4)
@@ -66,3 +72,20 @@ layout = html.Div(
     ],
     id="landing",
 )
+
+
+@app.callback(Output("world-medal-frame", "data"), Input("fun-facts", "value"))
+def load_medal_frame(value):
+    return create_medal_count_data_frame(os_data_raw).to_json()
+
+
+@app.callback(
+    Output("world-medal-race", "figure"),
+    Input("world-medal-frame", "data"),
+)
+def update_world_medal_race(data):
+    if data == None:
+        return px.bar()
+
+    sport_data_frame = pd.read_json(data)
+    return medal_race_plot(sport_data_frame)
