f34000791a479b70477f0938a637a787b590f82f
FilleFLEX <felix.tjernberg@hotmail.se>
2021-11-17 20:30:34 +0100

    Add parameterized navigation

--- dash_source/components/sport/sport.py
+++ dash_source/components/sport/sport.py
@@ -1,28 +1,28 @@
+import dash
 import pandas as pd
 import plotly_express as px
 from dash import dcc, html
 from dash.dependencies import Input, Output
+import time
 
 from app import app
 from components.sport.load_sport_data import load_sport_data_frames
 from components.sport.sport_list import sport_list
 import components.sport.plots as plots
 
-# print("sport ********************************************")
-sport_data_dictionaries = load_sport_data_frames()
 
+anatomy_filename = "https://raw.githubusercontent.com/Erik-M-Larsson/Databehandling-OS-projekt-Felix-Tjernberg-Erik-Larsson/de0f7d0a8dd874c37ca3ed00c72027bc72e9ef6c/dash_source/assets/anatomy.svg"
 
-anatomy_filename = "assets/anatomy.svg"
-
-medals_filename = "assets/os-medals.svg"
+medals_filename = "https://raw.githubusercontent.com/Erik-M-Larsson/Databehandling-OS-projekt-Felix-Tjernberg-Erik-Larsson/de0f7d0a8dd874c37ca3ed00c72027bc72e9ef6c/dash_source/assets/os-medals.svg"
 
 layout = html.Div(
     [
         dcc.Store(id="sport-data-frame"),
         dcc.Store(id="sport-medal-frame"),
+        # html.P(id="sport-name"),
         html.Header(
             [
-                html.H1(id="sport-name"),
+                html.H1(id="sport-name-heading"),
                 dcc.Dropdown(
                     clearable=False,
                     id="sport-dropdown",
@@ -65,7 +65,35 @@ layout = html.Div(
 )
 
 
-@app.callback(Output("sport-data-frame", "data"), Input("sport-dropdown", "value"))
+sport_data_dictionaries = load_sport_data_frames()
+
+
+def get_sport_name(string):
+    try:
+        return string.split("/")[2].replace("-", " ").title()
+    except:
+        return None
+
+
+def match_sport_list(string):
+    return any([string == sport_name for sport_name in sport_list])
+
+
+@app.callback(
+    Output("url", "pathname"),
+    Input("sport-dropdown", "value"),
+    prevent_initial_call=True,
+)
+def navigate_to_sport(value):
+    if any([value == sport for sport in sport_list]):
+        return f"/sport-stats/{value.lower().replace(' ', '-')}"
+
+    return dash.no_update
+
+
+@app.callback(
+    Output("sport-data-frame", "data"), Input("sport-name", "sport-name-test")
+)
 def load_data_frame(value):
     if value == None:
         return
@@ -73,7 +101,7 @@ def load_data_frame(value):
     return sport_data_dictionaries[f"{format(value.title())}"]["general"].to_json()
 
 
-@app.callback(Output("sport-medal-frame", "data"), Input("sport-dropdown", "value"))
+@app.callback(Output("sport-medal-frame", "data"), Input("sport-name", "children"))
 def load_medal_frame(value):
     if value == None:
         return
@@ -81,12 +109,18 @@ def load_medal_frame(value):
     return sport_data_dictionaries[f"{format(value.title())}"]["medal_count"].to_json()
 
 
-@app.callback(Output("sport-name", "children"), Input("sport-dropdown", "value"))
-def update_heading_text(value):
-    if value == None:
+@app.callback(
+    Output("sport-name-heading", "children"),
+    Input("url", "pathname"),
+    Input("height-and-width-histograms", "children"),
+    # prevent_initial_call=True,
+)
+def update_heading_text(value, children):
+    time.sleep(1)  # Hack to make callback load last
+    if not match_sport_list(get_sport_name(value)):
         return "Sport stats"
 
-    return value
+    return get_sport_name(value)
 
 
 @app.callback(
--- dash_source/index.py
+++ dash_source/index.py
@@ -6,7 +6,7 @@ from app import app
 from components.sport import sport
 from components.landing import landing
 
-os_rings_filename = "assets/os-rings.svg"
+os_rings_filename = "https://raw.githubusercontent.com/Erik-M-Larsson/Databehandling-OS-projekt-Felix-Tjernberg-Erik-Larsson/de0f7d0a8dd874c37ca3ed00c72027bc72e9ef6c/dash_source/assets/os-rings.svg"
 
 app.layout = html.Div(
     [
@@ -23,7 +23,7 @@ app.layout = html.Div(
         ),
         html.Main(
             [
-                dcc.Location(id="url", refresh=False),
+                dcc.Location(id="url"),
                 html.Article(id="page-content"),
             ]
         ),
@@ -34,7 +34,7 @@ app.title = "Olympic sports dash app"
 
 @app.callback(Output("page-content", "children"), Input("url", "pathname"))
 def display_page(path_name):
-    if path_name == "/sport-stats":
+    if path_name.split("/")[1] == "sport-stats":
         return sport.layout
     else:
         return landing.layout
